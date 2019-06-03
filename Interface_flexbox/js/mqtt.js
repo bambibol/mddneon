e88586508c905c3551cd06641815e24aee918162
Bambi <32299469+bambibol@users.noreply.github.com>
2019-06-03 21:07:51 +0200

    updated gradients + toggle button

--- Interface_flexbox/js/mqtt.js
+++ Interface_flexbox/js/mqtt.js
@@ -1,51 +1,52 @@
 // MQTT LISTENER
-
-
-// // Note: This line is only required when running in node.js
-// var mqtt = require('mqtt');
-
-$(function(){
+$(function() {
   var client = mqtt.connect('wss://try:try@broker.shiftr.io', {
     clientId: 'javascript'
   });
-
   client.subscribe('/mddneon');
-
-  client.on('connect', function(){
+  client.on('connect', function() {
     console.log('client 2 has connected!');
   });
-
   client.on('message', function(topic, message) {
     console.log('new message:', topic, message.toString());
   });
-
   // buttons:
-
-  $('#buttonR').click(function(){
+  $('#buttonR').click(function() {
     client.publish('/mddneon', 'R');
   })
-  $('#buttonE').click(function(){
+  $('#buttonE').click(function() {
     client.publish('/mddneon', 'e');
   })
-   $('#buttonr').click(function(){
+  $('#buttonr').click(function() {
     client.publish('/mddneon', 'r');
   })
-    $('#buttons').click(function(){
+  $('#buttons').click(function() {
     client.publish('/mddneon', 's');
   })
-     $('#buttonS').click(function(){
+  $('#buttonS').click(function() {
     client.publish('/mddneon', 'S');
   })
-      $('#buttonn').click(function(){
+  $('#buttonn').click(function() {
     client.publish('/mddneon', 'n');
   })
+  // color picker:
+  $('#square').on("change", function(e) {
+    var msg = '0x' + e.detail;
+    client.publish('/mddneon', msg);
+  });
+})
 
-      $('#square').on("change", function(e) {
-        //var t = target;
-        var msg = '0x'+e.detail;
-        //console.log(msg);
-        client.publish('/mddneon', msg);
-        //console.log("change!");
-      });
-
+$(function toggle() {
+  var x = document.getElementById("on");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var y = document.getElementById("off");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+  } else {
+    y.style.display = "block";
+  }
 })
